7498d9fb81f91d47c26e357c38dc21d32c37d118
KangHyun_Kim <162311939+hmmumm@users.noreply.github.com>
2024-05-03 17:09:23 +0900

    폴더 구조화 템플릿 파일 삭제
    필요없는 위치에 맞지 않는 파일 삭제
    ## 삭제 목록
    - License
    - package-lock.json
    - package.json
    - PRO_UPGRADE.txt
    - README.md
    - gulpfile.js
    
    ## 폴더 구조화
    static폴더와 templates로 나누어 html파일과 js, css 등등 나뉨

--- src/main/resources/static/js/component/sidebar.js
+++ src/main/resources/static/js/component/sidebar.js
@@ -1,14 +1,20 @@
 class sidebar extends HTMLElement {
-
+    
     connectedCallback() {
+       const path = window.location.pathname;
+       let sPath = "";
+       
+       if(path.indexOf("templates") > 0)
+            sPath = "../static/";
+
        this.innerHTML = `
        <!-- Sidebar -->
        <ul class="navbar-nav bg-dong sidebar sidebar-dark accordion " id="accordionSidebar">
 
            <!-- Sidebar - Brand -->
-           <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+           <a class="sidebar-brand d-flex align-items-center justify-content-center" href="${sPath}index.html">
                <div class="sidebar-brand-icon">
-                   <img src="img/main_icon.svg" />
+                   <img src="${sPath}img/main_icon.svg" />
                </div>
                <!-- <div class="sidebar-brand-text mx-3"><b1 class="text-black-50">OTL</b1></div> -->
            </a>
@@ -18,21 +24,21 @@ class sidebar extends HTMLElement {
            <!-- Nav Item - Dashboard -->
            <li class="nav-item">
                <a class="nav-link list-group-item-action list-group-item-danger rounded text-gray-900"
-                   href="index.html">
+                   href="${sPath}index.html">
                    <i class="fas fa-fw fa-th-large"></i>
                    <span>대시보드</span></a>
            </li>
            <!-- Nav Item - Dashboard -->
            <li class="nav-item">
                <a class="nav-link list-group-item-action list-group-item-danger rounded text-gray-900"
-                   href="index.html">
+                   href="${sPath}index.html">
                    <i class="fas fa-fw fa-clipboard"></i>
                    <span>게시판</span></a>
            </li>
 
            <!-- Nav Item - Pages Collapse Menu -->
            <li class="nav-item">
-               <a class="nav-link list-group-item-action list-group-item-danger rounded text-gray-900" href="#"
+               <a class="nav-link list-group-item-action list-group-item-danger rounded text-gray-900" href="${sPath}#"
                    data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
                    <i class="fas fa-fw fa-users"></i>
                    <span>스터디룸</span>
@@ -40,8 +46,8 @@ class sidebar extends HTMLElement {
                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                    <div class="bg-white py-2 collapse-inner rounded">
                        <h6 class="collapse-header">나의 스터디룸</h6>
-                       <a class="collapse-item" href=""><img src="img/Property 1=linux.svg">수연이와 리눅스</a>
-                       <a class="collapse-item" href=""><img src="img/Property 1=spring.svg">강현과 봄남자</a>
+                       <a class="collapse-item" href="${sPath}"><img src="${sPath}img/Property 1=linux.svg">수연이와 리눅스</a>
+                       <a class="collapse-item" href="${sPath}"><img src="${sPath}img/Property 1=spring.svg">강현과 봄남자</a>
                    </div>
                </div>
            </li>
