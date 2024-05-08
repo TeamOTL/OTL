0a34aaaf351e311c31e4eb17094b4f2634383473
coding-salmon <73780359+coding-salmon@users.noreply.github.com>
2024-05-08 17:29:26 +0900

    커밋에러

---
+++ src/main/java/com/otl/otl/controller/MyStudyController.java
@@ -0,0 +1,22 @@
+package com.otl.otl.controller;
+
+
+import com.otl.otl.service.MyStudyService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mystudy")
+@Log4j2
+@RequiredArgsConstructor
+public class MyStudyController {
+//    private final MyStudyService myStudyService;
+
+
+
+
+
+
+}
