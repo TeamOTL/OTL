557a2f23f513bf64030b775815e6fee25e65db6d
99duuk <99duuk@gmail.com>
2024-05-14 15:11:13 +0900

    feat/ 컨버터 주석 추가

--- src/main/java/com/otl/otl/service/MemberStudyServiceImpl.java
+++ src/main/java/com/otl/otl/service/MemberStudyServiceImpl.java
@@ -1,23 +1,19 @@
 package com.otl.otl.service;
 
 import com.otl.otl.domain.*;
-import com.otl.otl.dto.InterestsDTO;
 import com.otl.otl.dto.MemberStudyDTO;
 import com.otl.otl.dto.MemberStudyProjection.MemberStudyProjectionImpl;
 import com.otl.otl.dto.StudyListDTO;
-import com.otl.otl.dto.TaskDTO;
 import com.otl.otl.repository.MemberStudyRepository;
 import com.otl.otl.service.converter.CustomConverters;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class MemberStudyServiceImpl implements MemberStudyService {
 
     private final MemberStudyRepository memberStudyRepository;
-
     private final CustomConverters customConverters;
 
     public MemberStudyServiceImpl(MemberStudyRepository memberStudyRepository, CustomConverters customConverters) {
@@ -25,18 +21,17 @@ public class MemberStudyServiceImpl implements MemberStudyService {
         this.customConverters = customConverters;
     }
 
-
+    // 컨버터
     public StudyListDTO ProjectionToDTO(MemberStudyProjectionImpl projection) {
         return customConverters.ProjectionToDTO(projection);
     }
-
     public StudyListDTO StudyToDto(Study study) {
         return customConverters.StudyToDto(study);
     }
-
     public MemberStudy MemberStudyDTOToDomain(MemberStudyDTO memberStudyDTO, MemberStudy memberStudy) {
         return customConverters.MemberStudyDTOToDomain(memberStudyDTO, memberStudy);
     }
+
 
 
     @Override
